a0bfb4639aa0350ddae911802e63d7df4a9b9afc
azemazer <a.laval16@laposte.net>
2023-06-06 13:45:45 +0200

    created multiple py files for testing as well as base ui

--- app.py
+++ app.py
@@ -1,35 +1,91 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
-from PyQt5.QtCore import QSize, Qt
-
-# Only needed for access to command line arguments
 import sys
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QComboBox,
+    QDateEdit,
+    QDateTimeEdit,
+    QDial,
+    QDoubleSpinBox,
+    QFontComboBox,
+    QLabel,
+    QLCDNumber,
+    QLineEdit,
+    QMainWindow,
+    QProgressBar,
+    QPushButton,
+    QRadioButton,
+    QSlider,
+    QSpinBox,
+    QTimeEdit,
+    QVBoxLayout,
+    QWidget, 
+    QHBoxLayout
+)
+from PyQt5.QtGui import QPalette, QColor
+
+class Color(QWidget):
+
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(color))
+        self.setPalette(palette)
 
 # Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("My App")
+        self.setWindowTitle("HEAVY WEAPONS GUY")
 
-        button = QPushButton("Press Me!")
+        layoutH = QVBoxLayout()
+        layout1 = QHBoxLayout()
+        layout2 = QHBoxLayout()
+        layout3 = QHBoxLayout()
 
-        self.setFixedSize(QSize(1200, 900))
+        widgets1 = [
+            QCheckBox,
+            QPushButton,
+            QLabel
+        ]
+        widgets2 = [
+            QPushButton,
+            QProgressBar,
+            QProgressBar,
+            QPushButton
+        ]
 
-        # Set the central widget of the Window.
-        self.setCentralWidget(button)
+        for w in widgets1:
+            layout1.addWidget(w())
 
-# You need one (and only one) QApplication instance per application.
-# Pass in sys.argv to allow command line arguments for your app.
-# If you know you won't use command line arguments QApplication([]) works too.
+        layoutH.addLayout( layout1 )
+
+        for w in widgets2:
+            layout2.addWidget(w())
+
+        layoutH.addLayout( layout2 )
+
+        layoutH.setContentsMargins(20,20,20,20)
+        layoutH.setSpacing(40)
+
+        layout1.setSpacing(40)
+        layout2.setSpacing(40)
+
+        widget = QWidget()
+        widget.setLayout(layoutH)
+
+        # Set the central widget of the Window. Widget will expand
+        # to take up all the space in the window by default.
+        self.setCentralWidget(widget)
+
+
 app = QApplication(sys.argv)
-
-# Create a Qt widget, which will be our window.
 window = MainWindow()
-window.show()  # IMPORTANT!!!!! Windows are hidden by default.
+window.show()
 
-# Start the event loop.
 app.exec()
-
-
-# Your application won't reach here until you exit and the event
-# loop has stopped.
